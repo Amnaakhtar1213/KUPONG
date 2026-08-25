de52019618e3392d8c0d013bedaea8a1134ed8a3
amnaakhtar1213 <amnabinterasheed1911@gmail.com>
2026-08-25 22:50:15 +0500

    add fashion page

--- src/components/Electronics/ElectronicsHead.jsx
+++ src/components/Electronics/ElectronicsHead.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const ElectronicsHead = () => {
   return (
     <div className="">
-      <div className="  md:h-140 rounded-xl mt-50 md:mt-20">
+      <div className="  md:h-140 rounded-xl mt-54 md:mt-34">
           <img src="https://static.coupangcdn.com/va/cmg_paperboy/image/1787105344877/P-Top-R1-PC.jpg" className="rounded-xl w-full h-full object-contain"/>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mt-2">
           <img src="https://static.coupangcdn.com/ha/cmg_paperboy/image/1787114818951/P-Sub_PC.jpg" className=""/>
         </div>
     </div>
