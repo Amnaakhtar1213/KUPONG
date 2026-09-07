ff5a0497b06bacf18e4f2a7cea063a321cc6f6f0
amnaakhtar1213 <amnabinterasheed1911@gmail.com>
2026-09-07 12:45:43 +0500

    add wishlist logic to electronics

--- src/components/Electronics/ElectronicsSec.jsx
+++ src/components/Electronics/ElectronicsSec.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ElectronicsSec = ({product}) => {
+
+  const [isFavorite, setIsFavorite] = useState(() => {
+    const kupongSavedProducts = JSON.parse(localStorage.getItem("kupongSavedProducts")) || [];
+
+    return kupongSavedProducts.some((savedProduct) => savedProduct.id === product.id)
+  })
+
+  const handleSave = () => {
+    const kupongSavedProducts = JSON.parse(localStorage.getItem("kupongSavedProducts")) || [];
+    const isAlreadySaved = kupongSavedProducts.some((savedProduct) => savedProduct.id === product.id)
+
+    if(isAlreadySaved){
+      const updatedProducts = kupongSavedProducts.filter((savedProduct) => savedProduct.id !== product.id)
+      localStorage.setItem("kupongSavedProducts", JSON.stringify(updatedProducts))
+      setIsFavorite(false)
+    } else {
+      kupongSavedProducts.push(product)
+      localStorage.setItem("kupongSavedProducts", JSON.stringify(kupongSavedProducts))
+      setIsFavorite(true)
+    }
+  }
   return (
     <div className="p-4 flex flex-row md:flex-col w-120 md:w-70 hover:shadow-xl hover:shadow-gray-300">
       <div className="relative w-60">
         <img src={product.image} className="w-full h-full object-cover"/>
-        <i className="absolute bottom-2 right-2 text-gray-300 fa-solid fa-heart"></i>
+        <i onClick={handleSave} className={`absolute bottom-2 right-2 ${isFavorite ? "text-red-700" : "text-gray-300"} fa-solid fa-heart`}></i>
       </div>
       <div className="pl-2 mt-4">
         <h1 className="">{product.title}</h1>
